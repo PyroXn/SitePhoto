70305c0c9961caeb611e00d56c52f2fead596ae6
Florian <florian.janson@gmail.com>
2011-08-05 17:21:05 +0200

    Exemple de code PhP. A LIRE !

---
+++ Exemple code.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Permet de faire une somme de deux chiffres
+ * @param int $a Premier chiffre
+ * @param int $b Second chiffre
+ * @return Somme des deux chiffres
+**/
+function somme($a,$b) {
+    return $a + $b;
+}
+
+// Permet d'avoir la description de la fonction somme
+somme(42,12);
+
+?>
