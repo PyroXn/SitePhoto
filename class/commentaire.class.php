6fd05e035c417cb36c6c25d2f4d634605a222687
Florian <florian.janson@gmail.com>
2011-08-07 15:18:58 +0200

    modification classe commentaire

--- class/commentaire.class.php
+++ class/commentaire.class.php
@@ -1,9 +1,61 @@
 <?php
 
-class commentaires {
-    var $pseudo;
-    var $message;
-    var $timestamp;
+class commentaire {
+    private $pseudo;
+    private $message;
+    private $timestamp;
+    
+/*   CONSTRUCTOR    */
+    
+    function __construct($pseudo,$message,$timestamp) {
+        $this->pseudo = $pseudo;
+        $this->message = $message;
+        $this->timestamp = $timestamp;
+    }
+    
+/*   GETTER    */
+    
+    function getPseudo() {
+        return $this->pseudo;
+    }
+    function getMessage() {
+        return $this->message;
+    }
+    function getTimeStamp() {
+        return $this->timestamp;
+    }
+    
+/*   SETTER    */
+    
+    function setPseudo($pseudo) {
+        $this->pseudo = $pseudo;
+    }
+    function setMessage($message) {
+        $this->message = $message;
+    }
+    function setTimestamp($timestamp) {
+        $this->timestamp = $timestamp;
+    }
+    
+/*   METHODES    */
+    
+    /**
+     * Permet de sécuriser le message
+     */
+    function isSecurise() {
+        if (get_magic_quotes_gpc()) {
+            $this->message = stripslashes($this->message);
+        }
+        $this->message = mysql_real_escape_string($this->message);
+    }
+    
+    /**
+     * Permet d'insérer un nouveau commentaire
+     */
+    function isSubmit() {
+        $sql = "INSERT INTO commentaires(pseudo,message,timestamp) VALUES ('".$this->pseudo."','".$this->message."','".$this->timestamp."')";
+        $req = mysql_query($sql);
+    }
     
 }
 ?>
