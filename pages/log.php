ebf0529f4e323f6aa42ad9d2b2430d6e1620421d
Florian <florian.janson@gmail.com>
2011-08-16 16:05:22 +0200

    Ajout de mon design

---
+++ pages/log.php
@@ -0,0 +1,11 @@
+<?php
+// Fichier regroupant les pages inscription, connexion, oublie mot de passe...
+
+function inscription() {
+    $titre = "Devenez photographe !";
+    $content = "<h1>Créez votre compte !</h1>";
+    $content .= "<label name='mail'><input type='text' name='mail' />";
+    $content .= "<label name='password'><input type='password' name='password' />";
+    
+}
+?>
