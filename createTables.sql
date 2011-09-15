5fb502f0cca2739cc9f8bc7aaee0212734aa7153
Florian <florian.janson@gmail.com>
2011-09-15 21:07:35 +0200

    ajout sql

--- createTables.sql
+++ createTables.sql
@@ -3,12 +3,33 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Serveur: localhost
--- Généré le : Lundi 12 Septembre 2011 à 19:23
+-- Généré le : Jeudi 15 Septembre 2011 à 21:06
 -- Version du serveur: 5.0.27
 -- Version de PHP: 5.2.1
 -- 
 -- Base de données: `sitephoto`
 -- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Structure de la table `albums`
+-- 
+
+CREATE TABLE `albums` (
+  `id` int(10) NOT NULL auto_increment,
+  `titre` varchar(60) NOT NULL,
+  `idMembres` int(10) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- 
+-- Contenu de la table `albums`
+-- 
+
+INSERT INTO `albums` (`id`, `titre`, `idMembres`) VALUES 
+(1, 'Animaux', 11),
+(2, 'Ciboulette', 11);
 
 -- --------------------------------------------------------
 
@@ -42,6 +63,7 @@ CREATE TABLE `concours` (
   `titre` varchar(80) NOT NULL,
   `description` longtext NOT NULL,
   `nbParticipant` int(10) NOT NULL default '0',
+  `url` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
@@ -49,8 +71,8 @@ CREATE TABLE `concours` (
 -- Contenu de la table `concours`
 -- 
 
-INSERT INTO `concours` (`id`, `titre`, `description`, `nbParticipant`) VALUES 
-(1, 'Le mois d''août, la couleur jaune !', 'Le moi d’août, le mois des mirabelles ! C''est donc pour cela que nous avons tout simplement décidé de vous proposer un concours sur le thème de la couleur jaune. Bien entendu, il est interdit de retoucher les photos via l''ordinateur. A gagner, ce mois-ci : Un abonnement à la revue XY spécial photographe amateur, un abonement gold d''une durée de 3 mois et un abonement gold d''une durée d''un mois. Bon concours à tous ! ', 0);
+INSERT INTO `concours` (`id`, `titre`, `description`, `nbParticipant`, `url`) VALUES 
+(1, 'Le mois d''août, la couleur jaune !', 'Le moi d’août, le mois des mirabelles ! C''est donc pour cela que nous avons tout simplement décidé de vous proposer un concours sur le thème de la couleur jaune. Bien entendu, il est interdit de retoucher les photos via l''ordinateur. A gagner, ce mois-ci : Un abonnement à la revue XY spécial photographe amateur, un abonement gold d''une durée de 3 mois et un abonement gold d''une durée d''un mois. Bon concours à tous ! ', 0, './templates/images/img.jpeg');
 
 -- --------------------------------------------------------
 
@@ -68,12 +90,7 @@ CREATE TABLE `images` (
   `idAlbum` int(10) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `fk_images_membres` (`idMembres`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- 
--- Contenu de la table `images`
--- 
-
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -91,7 +108,7 @@ CREATE TABLE `membres` (
   `birthday` date default NULL,
   `cle` varchar(55) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 -- 
 -- Contenu de la table `membres`
@@ -99,5 +116,5 @@ CREATE TABLE `membres` (
 
 INSERT INTO `membres` (`id`, `pseudo`, `password`, `mail`, `sexe`, `avatar`, `birthday`, `cle`) VALUES 
 (8, 'Zoz', 'ee8e921499f6b8051b7c6667985b4e62', 'florian.janson@gmail.com', '1', '', '1989-11-07', '224a1638f54243b5bccd8e6c9a38d3bb'),
-(10, '', 'd41d8cd98f00b204e9800998ecf8427e', '', '2', '', '0000-00-00', '7ee143a945eff5b2ec596580559c6937'),
-(11, 'Flo', '56910c52ed70539e3ce0391edeb6d339', 'heberg.disocunt@gmail.com', '1', '', '1917-01-18', '89000665051d12e173f1e56ebb5cbdb0');
+(11, 'Flo', '56910c52ed70539e3ce0391edeb6d339', 'heberg.disocunt@gmail.com', '1', '', '1917-01-18', '89000665051d12e173f1e56ebb5cbdb0'),
+(12, 'Zozor', '56910c52ed70539e3ce0391edeb6d339', 'toto@toto.com', '1', '', '1989-11-07', '84df3e139094919a562cc8c4ae6f8e83');
