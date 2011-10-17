350029ff865c06081bee0298eb45816285bea509
Florian <florian.janson@gmail.com>
2011-10-17 11:52:17 +0200

    bug

--- templates/haut.php
+++ templates/haut.php
@@ -30,7 +30,7 @@
     <body>
         <!--[if lte IE 8]>
   <div class="alert-ie6" style="padding: 1em; background: #900; font-size: 1.1em; color: #fff;">
-    <p><strong>Attention ! </strong> Votre navigateur (Internet Explorer 6 ou 7) présente de sérieuses lacunes en terme de sécurité et de performances, dues à son obsolescence (il date de 2001).<br />En conséquence, ce site sera consultable mais de manière moins optimale qu'avec un navigateur récent (<a href="http://www.browserforthebetter.com/download.html" style="color: #fff;">Internet Explorer 9+</a>, <a href="http://www.mozilla-europe.org/fr/firefox/" style="color: #fff;">Firefox 4+</a>, <a href="http://www.google.com/chrome?hl=fr" style="color: #fff;">Chrome 11+</a>, <a href="http://www.apple.com/fr/safari/download/" style="color: #fff;">Safari 5+</a>,...)</p>
+    <p><strong>Attention ! </strong> Votre navigateur (Internet Explorer) présente de sérieuses lacunes en terme de sécurité et de performances, dues à son obsolescence.<br />En conséquence, ce site sera consultable mais de manière moins optimale qu'avec un navigateur récent (<a href="http://www.browserforthebetter.com/download.html" style="color: #fff;">Internet Explorer 9+</a>, <a href="http://www.mozilla-europe.org/fr/firefox/" style="color: #fff;">Firefox 4+</a>, <a href="http://www.google.com/chrome?hl=fr" style="color: #fff;">Chrome 11+</a>, <a href="http://www.apple.com/fr/safari/download/" style="color: #fff;">Safari 5+</a>,...)</p>
   </div>
 <![endif]-->
         <div id="header">
