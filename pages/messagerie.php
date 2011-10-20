3d93c2d60d0e117af4bca6c4b276136fcaa9dd89
Florian <florian.janson@gmail.com>
2011-10-20 11:44:29 +0200

    messagerie

--- pages/messagerie.php
+++ pages/messagerie.php
@@ -8,6 +8,21 @@ function messagerie() {
     $title = 'Pixels Arts - Messagerie : Accueil';
     $contenu = menuLeft($_SESSION['user']);
     $contenu .= '<h1>Messagerie</h1>';
+    
+    $user = $_SESSION['user'];
+    $messagesRecus = getMessagesRecus($user->getId());
+    $contenu .= '<ul>';
+    if ($messagesRecus != null) {
+        foreach($messagesRecus as $messageRecus){
+            $contenu .= '
+                <li class="message">
+                    '.$user->getPseudoFormat().'
+                </li>';
+        }   
+    }
+    $contenu .= '</ul>';
+    
+    
     $contenu .= mosaique();
     display($title,$contenu);
     
